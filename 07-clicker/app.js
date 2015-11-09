1b455245b8903995aa8cda7f9e4c78bb828bb530
Brett Lyons <blyons@students.naropa.edu>
2015-11-08 19:10:57 -0700

    delinted 07-clicker/app.js, started redditclone/app.js

--- 07-clicker/app.js
+++ 07-clicker/app.js
@@ -1,10 +1,10 @@
 angular.module('clickerApp',[])
   .controller('clickerApp', function($scope) {
-    $scope.greeting = "Yooheeeelllooo";
-    $scope.pickRandomNumber = function(){
-      $scope.number = Math.floor(Math.random() * 10) + 1
-    }
-    $scope.reverseWord = function(someInfo){
+    $scope.greeting = 'Yooheeeelllooo';
+    $scope.pickRandomNumber = function() {
+      $scope.number = Math.floor(Math.random() * 10) + 1;
+    };
+    $scope.reverseWord = function() {
       $scope.theWord = $scope.theWord.split('').reverse().join('');
-    }
-  })
+    };
+  });
---
+++ 08-redditclone/app.js
@@ -0,0 +1,4 @@
+angular.module('redditClone',[])
+  .controller('redditClone', function($scope) {
+    $scope.placeholder = 0;
+  });
